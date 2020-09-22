4e66ca5d8a54f82574355f21eec83a3b53a08c64
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2020-09-22 16:25:16 -0400

    Update install.sh
    change back to forward on slashes

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-printf "Start of Script File /n" > DotfilesLog.txt
+printf "Start of Script File \n" > DotfilesLog.txt
 
-printf $"$PWD =  {$PWD} /n" > DotfilesLog.txt
+printf $"$PWD =  {$PWD} \n" >> DotfilesLog.txt
 ## copy git files to Codespace Project Home directory
 
 cp ~/dotfiles/.gitignore $PWD
 cp ~/dotfiles/.gitconfig $PWD
 
-printf "End of Script File /n" >> DotfilesLog.txt
+printf "End of Script File \n" >> DotfilesLog.txt
