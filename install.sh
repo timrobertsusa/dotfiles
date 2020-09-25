96bc617fa0b9c58fc605728ddf77f5e4ae91aed9
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2020-09-25 13:51:23 -0400

    Update install.sh
    install pwsh as a personal customization

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 printf "Start of Script File \n" > DotfilesLog.txt
 
 printf "PWD= " {$PWD} "\n" >> DotfilesLog.txt
-## copy git files to Codespace Project Home directory
 
-cp ~/dotfiles/.gitignore $PWD
-cp ~/dotfiles/.gitconfig $PWD
+
+## copy git files to Codespace Project Home directory
+##  cp ~/dotfiles/.gitignore $PWD
+##  cp ~/dotfiles/.gitconfig $PWD
+
+
+## Update to the latest PowerShell
+curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
 
 printf "End of Script File \n" >> DotfilesLog.txt
