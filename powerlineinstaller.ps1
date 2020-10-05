cece5ce8b53fe0ddecd510778789bd66a4f2e682
Tim Roberts <timrobertsusa@pluralpoint.com>
2020-10-05 12:22:20 -0400

    Create powerlineinstaller.ps1

---
+++ powerlineinstaller.ps1
@@ -0,0 +1,4 @@
+Install-Module posh-git -Scope CurrentUser -Force -SkipPublisherCheck
+Install-Module oh-my-posh -Scope CurrentUser -Force -SkipPublisherCheck
+
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
