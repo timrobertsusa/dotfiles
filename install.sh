3ad90f3c57be3c55681871e31004bac94ea60bb6
Tim Roberts <timrobertsusa@pluralpoint.com>
2020-10-06 14:06:32 -0400

    Update install.sh
    added more log entries

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ pwsh ~/dotfiles/powerlineinstaller.ps1
 
 mkdir -p /root/.config/powershell/ && cp ~/dotfiles/mypwshprofile.ps1 /root/.config/powershell/Microsoft.Powershell_profile.ps1
 cp ~/dotfiles/mypwshprofile.ps1 /root/.config/powershell/profile.ps1
-
+printf "PWSH Powerline Complete \n" >> DotfilesLog.txt
 ## For powerline bash
 cd ~
 wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
@@ -28,13 +28,12 @@ tar xvf go1.15.2.linux-amd64.tar.gz
 mv go /usr/local
 md go 
 mkdir go
-
 rm go1.15.2.linux-amd64.tar.gz
- 
+printf "Go Downloaded \n" >> DotfilesLog.txt
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH 
- 
+printf "Go PATHS SET \n" >> DotfilesLog.txt 
 cd ~
 go get -u github.com/justjanne/powerline-go
 echo 'function _update_ps1() {
@@ -43,7 +42,5 @@ echo 'function _update_ps1() {
 if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
 fi' >>/root/.bashrc
-
 printf "End of bash powerline Script File \n" >> DotfilesLog.txt
-
 printf "End of Script File \n" >> DotfilesLog.txt
