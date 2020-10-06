c06cc28147c2ac21cd5ec371b51e8a77973a4712
Tim Roberts <timrobertsusa@pluralpoint.com>
2020-10-06 17:51:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
-printf "\n***Start of Script File*** \n" > DotfilesLog.txt
+printf "Start of Script File \n" > ~/dotfiles/DotfilesLog.txt 
 
-printf '\n PWD='$PWD \n >> DotfilesLog.txt
+printf "PWD= $PWD \n" >> ~/dotfiles/DotfilesLog.txt 
 
-##printf "$DOGVAR \n">> DotfilesLog.txt
-printf '\n DOGVAR= '$DOGVAR \n >> DotfilesLog.txt
+printf "DOGVAR= $DOGVAR \n">> ~/dotfiles/DotfilesLog.txt 
 
-printf 'Local Workspace = ' $localWorkspaceFolder \n >> DotfilesLog.txt
-printf 'Container Workspace = ' $containerWorkspaceFolder \n >> DotfilesLog.txt
+printf "Local Workspace = $localWorkspaceFolder \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "Container Workspace = $containerWorkspaceFolder \n" >> ~/dotfiles/DotfilesLog.txt 
 
 
 ## copy git files to Codespace Project Home directory
@@ -17,43 +16,53 @@ printf 'Container Workspace = ' $containerWorkspaceFolder \n >> DotfilesLog.txt
 ## Update to the latest PowerShell
 curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
 
-pwsh ~/dotfiles/powerlineinstaller.ps1
+pwsh cmdprmt.ps1
 
-mkdir -p /root/.config/powershell/ && cp ~/dotfiles/mypwshprofile.ps1 /root/.config/powershell/Microsoft.Powershell_profile.ps1
-cp ~/dotfiles/mypwshprofile.ps1 /root/.config/powershell/profile.ps1
+mkdir -p /root/.config/powershell/ && cp ~/dotfiles/myprofile.ps1 /root/.config/powershell/Microsoft.PowerShell_profile.ps1
 
-printf "***PWSH Powerline Complete \n" >> DotfilesLog.txt
+printf "Start of bash powerline \n" >> ~/dotfiles/DotfilesLog.txt 
 
-## For powerline bash
 cd ~
 wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
 tar xvf go1.15.2.linux-amd64.tar.gz
 mv go /usr/local
-md go 
 mkdir go
+
 rm go1.15.2.linux-amd64.tar.gz
 
-printf "Go Downloaded \n" >> DotfilesLog.txt
-printf "Setting GO PATHs \n" >> DotfilesLog.txt
-
+printf "wget, tar, etc  complete \n" >> ~/dotfiles/DotfilesLog.txt 
+	
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH 
 
-printf "Go PATHS SET \n" >> DotfilesLog.txt 
-printf '\n HOME= '$HOME \n >> DotfilesLog.txt
-printf '\n GOROOT= '$GOROOT \n >> DotfilesLog.txt
-printf '\n GOPATH= '$GOPATH \n >> DotfilesLog.txt
-printf '\n PATH= '$PATH \n >> DotfilesLog.txt
+printf "GOROOT = $GOROOT \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "GOPATH =  $GOPATH \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "PATH =  $PATH \n" >> ~/dotfiles/DotfilesLog.txt 
 
-printf "Setting up bash powerline theme \n" >> DotfilesLog.txt 
+printf "export complete  \n" >> ~/dotfiles/DotfilesLog.txt 
+
 cd ~
 go get -u github.com/justjanne/powerline-go
+
+printf "powerline complete  \n" >> ~/dotfiles/DotfilesLog.txt 
+
+echo 'export GOROOT=/usr/local/go' >>/root/.bashrc
+echo 'export GOPATH=$HOME/go' >>/root/.bashrc
+echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH' >>/root/.bashrc
+
+printf "GOROOT = $GOROOT \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "GOPATH =  $GOPATH \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "PATH =  $PATH \n" >> ~/dotfiles/DotfilesLog.txt 
+
 echo 'function _update_ps1() {
     PS1="$($GOPATH/bin/powerline-go -error $?)"
 }
 if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
-fi' >> /root/.bashrc
-printf "End of bash powerline Script File \n" >> DotfilesLog.txt
-printf "End of Script File \n" >> DotfilesLog.txt
+fi' >>/root/.bashrc
+
+
+printf "bashrc complete  \n" >> ~/dotfiles/DotfilesLog.txt 
+
+printf "End of Script File \n" >> ~/dotfiles/DotfilesLog.txt 
