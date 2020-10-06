9625f14fd37d9877edbad013e7610f2d5eb5d28e
Tim Roberts <timrobertsusa@pluralpoint.com>
2020-10-06 15:13:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-printf "Start of Script File \n" > DotfilesLog.txt
+printf "\n***Start of Script File*** \n" > DotfilesLog.txt
 
-printf "PWD= $PWD \n" >> DotfilesLog.txt
+printf '\n PWD='$PWD \n >> DotfilesLog.txt
 
-printf "$DOGVAR \n">> DotfilesLog.txt
+##printf "$DOGVAR \n">> DotfilesLog.txt
+printf '\n DOGVAR='$DOGVAR \n >> DotfilesLog.txt
 
-printf "Local Workspace = $localWorkspaceFolder \n" >> DotfilesLog.txt
-printf "Container Workspace = $containerWorkspaceFolder \n" >> DotfilesLog.txt
+printf 'Local Workspace = ' $localWorkspaceFolder \n >> DotfilesLog.txt
+printf 'Container Workspace = ' $containerWorkspaceFolder \n >> DotfilesLog.txt
 
 
 ## copy git files to Codespace Project Home directory
@@ -20,7 +21,9 @@ pwsh ~/dotfiles/powerlineinstaller.ps1
 
 mkdir -p /root/.config/powershell/ && cp ~/dotfiles/mypwshprofile.ps1 /root/.config/powershell/Microsoft.Powershell_profile.ps1
 cp ~/dotfiles/mypwshprofile.ps1 /root/.config/powershell/profile.ps1
-printf "PWSH Powerline Complete \n" >> DotfilesLog.txt
+
+printf "***PWSH Powerline Complete \n" >> DotfilesLog.txt
+
 ## For powerline bash
 cd ~
 wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
@@ -29,11 +32,16 @@ mv go /usr/local
 md go 
 mkdir go
 rm go1.15.2.linux-amd64.tar.gz
+
 printf "Go Downloaded \n" >> DotfilesLog.txt
+printf "Setting GO PATHs \n" >> DotfilesLog.txt
+
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH 
+
 printf "Go PATHS SET \n" >> DotfilesLog.txt 
+printf "Setting up bash powerline theme \n" >> DotfilesLog.txt 
 cd ~
 go get -u github.com/justjanne/powerline-go
 echo 'function _update_ps1() {
@@ -41,6 +49,6 @@ echo 'function _update_ps1() {
 }
 if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
-fi' >>/root/.bashrc
+fi' >> /root/.bashrc
 printf "End of bash powerline Script File \n" >> DotfilesLog.txt
 printf "End of Script File \n" >> DotfilesLog.txt
