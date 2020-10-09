b02eaa011ac6301ed138254b5913c20c1f2b4d50
Tim Roberts <timrobertsusa@pluralpoint.com>
2020-10-09 15:16:40 -0400

    Update install.sh
    bash pl only

--- install.sh
+++ install.sh
@@ -1,24 +1,45 @@
-printf "Start of Script File \n" > ~/dotfiles/DotfilesLog.txt 
+printf "Start of bash powerline \n" >> ~/dotfiles/DotfilesLog.txt 
+cd ~
+wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
+tar xvf go1.15.2.linux-amd64.tar.gz
+mv go /usr/local
+mkdir go
 
-printf "PWD= $PWD \n" >> ~/dotfiles/DotfilesLog.txt 
+rm go1.15.2.linux-amd64.tar.gz
 
-printf "DOGVAR= $DOGVAR \n">> ~/dotfiles/DotfilesLog.txt 
+printf "wget, tar, etc  complete \n" >> ~/dotfiles/DotfilesLog.txt 
+	
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH 
 
-printf "\n Local Workspace = $localWorkspaceFolder \n" >> ~/dotfiles/DotfilesLog.txt 
-printf "\n Container Workspace = $containerWorkspaceFolder \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "GOROOT = $GOROOT \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "GOPATH =  $GOPATH \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "PATH =  $PATH \n" >> ~/dotfiles/DotfilesLog.txt 
+
+printf "export complete  \n" >> ~/dotfiles/DotfilesLog.txt 
+
+cd ~
+go get -u github.com/justjanne/powerline-go
+
+printf "powerline complete  \n" >> ~/dotfiles/DotfilesLog.txt 
+
+echo 'export GOROOT=/usr/local/go' >>/root/.bashrc
+echo 'export GOPATH=$HOME/go' >>/root/.bashrc
+echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH' >>/root/.bashrc
+
+printf "GOROOT = $GOROOT \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "GOPATH =  $GOPATH \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "PATH =  $PATH \n" >> ~/dotfiles/DotfilesLog.txt 
+
+echo 'function _update_ps1() {
+    PS1="$($GOPATH/bin/powerline-go -error $?)"
+}
+if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi' >>/root/.bashrc
 
 
-## copy git files to Codespace Project Home directory
-##  cp ~/dotfiles/.gitignore $PWD
-##  cp ~/dotfiles/.gitconfig $PWD
+printf "bashrc complete  \n" >> ~/dotfiles/DotfilesLog.txt 
 
-
-## Update to the latest PowerShell
-curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
-
-pwsh powerlineinstaller.ps1
-
-## mkdir -p /root/.config/powershell/ && cp ~/dotfiles/mypwshprofile.ps1 /root/.config/powershell/Microsoft.PowerShell_profile.ps1
-mkdir -p /~/.config/powershell/ && cp ~/dotfiles/mypwshprofile.ps1 /~/.config/powershell/Microsoft.PowerShell_profile.ps1
-
-printf "\n pwsh powerline complete \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "End of Script File \n" >> ~/dotfiles/DotfilesLog.txt 
